a386ebc4d03f9b9dca704c21716dacd0a1d1185e
Nazgul <nazgulchan@gmail.com>
2023-04-07 19:28:38 +0600

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const container = document.getElementById('container');
 const recipeResult = document.getElementById('recipe-result');
 
 async function searchByIngredients(ingredients) {
-    const response = await fetch(${ apiUrl } / recipes / findByIngredients ? ${ ingredients } & number = 10 & limitLicense=true & ranking = 1 & ignorePantry=false & apiKey = 30099d303fac4ab0a08b3225e10e9123);
+    const response = await fetch("https://api.spoonacular.com/recipes/findByIngredients?ingredients=carrots,tomatoes&number=10&limitLicense=true&ranking=1&ignorePantry=false&apiKey=30099d303fac4ab0a08b3225e10e9123");
     const json = await response.json();
     return json;
 }
