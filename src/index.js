5084f69f1ca025ffd779408048bcb09792c73889
Ira Zh <irishazhgun@gmail.com>
2023-04-16 01:57:15 +0300

    add localStorage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 const stars = document.querySelectorAll('.star');
+
 function setRating(rating) {
-  // нужно будет доделать сохранение рейтинга
-  console.log(`Рейтинг установлен: ${rating}`);
+  localStorage.setItem('recipeRating', rating);
 }
+
+const savedRating = localStorage.getItem('recipeRating');
+if (savedRating) {
+  stars.forEach(star => {
+    if (star.getAttribute('data-value') == savedRating) {
+      star.classList.add('active');
+    }
+  });
+}
+
 stars.forEach(star => {
   star.addEventListener('click', () => {
     const rating = star.getAttribute('data-value');
     setRating(rating);
+    stars.forEach(s => {
+      if (s.getAttribute('data-value') <= rating) {
+        s.classList.add('active');
+      } else {
+        s.classList.remove('active');
+      }
+    });
   });
 });
